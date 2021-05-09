f93a25e5fde013195e52b7f8a5678a375b2a578e
RX1226 <0979097955s@gmail.com>
2021-05-09 16:30:53 +0800

    add download image error icon

--- src/app/src/main/java/com/rx/application/ui/fragment/HouseDetailFragment.kt
+++ src/app/src/main/java/com/rx/application/ui/fragment/HouseDetailFragment.kt
@@ -1,12 +1,14 @@
 package com.rx.application.ui.fragment
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.DataBindingUtil
@@ -19,6 +21,10 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.rx.application.MainActivity
 import com.rx.application.viewmodel.MainViewModel
 import com.rx.application.R
@@ -127,7 +133,17 @@ class HouseDetailFragment : Fragment() {
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val data = datas.get(position)
-            Glide.with(activity).load(data.pic1Url).into(holder.img)
+            Glide.with(activity).load(data.pic1Url).listener(object : RequestListener<Drawable>{
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    holder.img.scaleType = ImageView.ScaleType.CENTER
+                    holder.img.setImageResource(R.drawable.ic_error)
+                    return true
+                }
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    holder.img.scaleType = ImageView.ScaleType.CENTER_CROP
+                    return false
+                }
+            }).into(holder.img)
             holder.txt_name.text = data.name
             holder.txt_desc.text = data.alias
             holder.btn_forward.setOnClickListener {
