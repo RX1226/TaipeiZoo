36cfe7d3c794d2b620604a910ad5d0915490b1e4
RX1226 <0979097955s@gmail.com>
2021-05-26 13:37:02 +0800

    update Parcelable method

--- app/src/main/java/com/rx/application/model/House.kt
+++ app/src/main/java/com/rx/application/model/House.kt
@@ -1,8 +1,9 @@
 package com.rx.application.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class House(
     val no: Int?,
     val category: String?,
@@ -12,41 +13,4 @@ data class House(
     val memo: String?,
     val geo: String?,
     val url: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(no)
-        parcel.writeString(category)
-        parcel.writeString(name)
-        parcel.writeString(pic)
-        parcel.writeString(info)
-        parcel.writeString(memo)
-        parcel.writeString(geo)
-        parcel.writeString(url)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<House> {
-        override fun createFromParcel(parcel: Parcel): House {
-            return House(parcel)
-        }
-
-        override fun newArray(size: Int): Array<House?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
--- app/src/main/java/com/rx/application/model/Plant.kt
+++ app/src/main/java/com/rx/application/model/Plant.kt
@@ -1,8 +1,9 @@
 package com.rx.application.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Plant(
     val name: String?,
     val alias: String?,
@@ -20,57 +21,4 @@ data class Plant(
     val pic2Alt: String?,
     val pic2Url: String?,
     val lastUpdate: String?
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(alias)
-        parcel.writeString(geo)
-        parcel.writeString(location)
-        parcel.writeString(nameEnglish)
-        parcel.writeString(nameLatin)
-        parcel.writeString(family)
-        parcel.writeString(genus)
-        parcel.writeString(brief)
-        parcel.writeString(feature)
-        parcel.writeString(usefulness)
-        parcel.writeString(pic1Alt)
-        parcel.writeString(pic1Url)
-        parcel.writeString(pic2Alt)
-        parcel.writeString(pic2Url)
-        parcel.writeString(lastUpdate)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Plant> {
-        override fun createFromParcel(parcel: Parcel): Plant {
-            return Plant(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Plant?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
